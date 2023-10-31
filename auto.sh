a6048e2fcce3cde2c8edd4560faf4d8b76a503c2
yuandongx <yuandongx@126.com>
2023-10-31 14:17:45 +0800

    Auto-commit 20231031

--- auto.sh
+++ auto.sh
@@ -1,1 +1,10 @@
-git status
+nothing=$(git status | grep "nothing to commit")
+if [ -z $nothing ]
+then
+ git add .
+ _time=$(date +%Y%m%d)
+ git commit -m "Auto-commit $_time"
+ echo "Auto commit the files at $_time."
+else
+ echo "nothing to commit."
+fi
